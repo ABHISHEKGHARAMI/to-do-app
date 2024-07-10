b47d0a1f4548cfdb7af37772be858414f3f6e9e0
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-07-10 16:11:51 +0530

    .mod

---
+++ to_do_app/todo/forms.py
@@ -0,0 +1,21 @@
+# creating the form to register user
+
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Todo
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+    
+    
+    class Meta:
+        model = User
+        fields = ['username','email','password1','password2']
+        
+        
+class TodoForm(forms.ModelForm):
+    class Meta:
+        model = Todo
+        fields = ['title','description','completed']
+    
