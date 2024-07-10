ca865a5c4a72e29b17584af0d09bd1919a53befc
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-07-10 22:14:01 +0530

    this is the register view .

--- to_do_app/todo/views.py
+++ to_do_app/todo/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login,logout,authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import UserCreationForm,TodoForm
+from .models import Todo
 
 # Create your views here.
+
+# first registering the user
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username,password=password)
+            login(request,user)
+            return redirect('todo_list')
+            
+    else:
+        form = UserCreationForm()
+    
+    return register(request,
+                    'register.html',
+                    {
+                        'form':form
+                    }) 
