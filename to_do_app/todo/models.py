06c99182a8194d9e1f9761325c8e3832c8f8b26b
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-07-10 15:41:31 +0530

    Adding the model for the user .

--- to_do_app/todo/models.py
+++ to_do_app/todo/models.py
@@ -6,4 +6,26 @@ from django.contrib.auth.models import User
 class Todo(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    
+    class Meta:
+        ordering = ['-created_at']
+        indexes = [
+            models.Index(fields=['-created_at'])
+        ]
+        
+    def __str__(self):
+        return self.title
+    
+    
+class UserProfile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    bio = models.TextField()
+    
+    
+    def __str__(self):
+        return self.user.username
     
